d21c89451485d0a625987b8f199c46bbc117ba64
Saúl Yaguana <saulyaguanacruz@gmail.com>
2025-09-13 00:00:32 -0500

    First look at a class in python

---
+++ 01-PartI/01-procedural-python/07-Account.py
@@ -0,0 +1,40 @@
+# Account class
+
+class Account():
+    def __init__(self, name, balance, password):
+        self.name = name
+        self.balance = balance
+        self.password = password
+        
+    def deposit(self, amount_to_deposit, password):
+        if password != self.password:
+            print("Sorry, incorrect password")
+            return None
+        if amount_to_deposit < 0:
+            print("You cannot deposit a negative amount")
+            return None
+        self.balance += amount_to_deposit
+        return self.balance
+    
+    def withdraw(self, amount_to_withdraw, password):
+        if password != self.password:
+            print("Incorrect password for this account")
+            return None
+        if amount_to_withdraw < 0 or amount_to_withdraw > self.balance:
+            print("You cannot withdraw that amount")
+            return None
+        self.balance -= amount_to_withdraw
+        return self.balance
+
+    def get_balance(self, password):
+        if password != self.password:
+            print("Sorry, incorrect password")
+            return None
+        return self.balance
+    
+    # Added for debugging
+    def show(self):
+        print(f"Name: {self.name}")
+        print(f"Blance: {self.balance}")
+        print(f"Password: {self.password}")
+        print()
