c7fcaa6c80a0e1c3e6edc37e10018a43bfec272f
Saúl Yaguana <saulyaguanacruz@gmail.com>
2025-09-24 15:58:38 -0500

    Extended test program (version 1) asking for some input from the user

--- 01-PartI/04-managing-multiple-objects/bank_version1.py
+++ 01-PartI/04-managing-multiple-objects/bank_version1.py
@@ -4,6 +4,26 @@
 # Bring in all the code from the Account class file
 from Account import Account
 
+# Create another account with information from the user
+print()
+user_name = input("What is the name for the new user account?: ")
+user_balance = int(input("What is the starting balance for this account?: "))
+user_password = input("What is the password you want to use for this account?: ")
+
+new_account = Account(user_name, user_balance, user_password)
+
+# Show the newly created user account
+new_account.show()
+
+# Let's deposit 100 into the new account
+new_account.deposit(100, user_password)
+user_balance = new_account.get_balance(user_password)
+print()
+print(f"After depositing 100, the user's balance is: {user_balance}")
+
+# Show the new account
+new_account.show()
+
 # Create two accounts
 joes_account = Account("Joe", 100, "joesPassword")
 print("Created an account for Joe")
