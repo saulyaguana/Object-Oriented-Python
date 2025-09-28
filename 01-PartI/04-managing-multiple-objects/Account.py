b81d91afcc3cabb64404245f294ca8e911f9e0a0
Saúl Yaguana <saulyaguanacruz@gmail.com>
2025-09-28 17:16:13 -0500

    Modified Account.py using raise and try/except blocks

--- 01-PartI/04-managing-multiple-objects/Account.py
+++ 01-PartI/04-managing-multiple-objects/Account.py
@@ -1,35 +1,41 @@
 # Account class
 
+class AbortTransaction(Exception):
+    """raise this exception to abort a bank transaction"""
+    pass
+
 class Account():
     def __init__(self, name, balance, password):
         self.name = name
-        self.balance = balance
+        self.balance = self.validate_amount(balance)
         self.password = password
-        
-    def deposit(self, amount_to_deposit, password):
+    
+    def validate_amount(self, amount):
+        try:
+            amount = int(amount)
+        except ValueError:
+            raise AbortTransaction("Amount must be an integer")
+        if amount <= 0:
+            raise AbortTransaction("Amount must be positive")
+        return amount
+    
+    def check_password_match(self, password):
         if password != self.password:
-            print("Sorry, incorrect password")
-            return None
-        if amount_to_deposit < 0:
-            print("You cannot deposit a negative amount")
-            return None
+            raise AbortTransaction("Incorrect password for this account")
+    
+    def deposit(self, amount_to_deposit):
+        amount_to_deposit = self.validate_amount(amount_to_deposit)
         self.balance += amount_to_deposit
         return self.balance
     
-    def withdraw(self, amount_to_withdraw, password):
-        if password != self.password:
-            print("Sorry, incorrect password")
-            return None
-        if amount_to_withdraw < 0 or amount_to_withdraw > self.balance:
-            print("You cannot withdraw that amount")
-            return None
+    def withdraw(self, amount_to_withdraw):
+        amount_to_withdraw = self.validate_amount(amount_to_withdraw)
+        if amount_to_withdraw > self.balance:
+            raise AbortTransaction("You cannot withdraw more than you have in your account")
         self.balance -= amount_to_withdraw
         return self.balance
     
-    def get_balance(self, password):
-        if password != self.password:
-            print("Sorry, incorrect password")
-            return None
+    def get_balance(self):
         return self.balance
     
     # Added for debugging
